3b069d4f8afb6b696b62c7d2417d77513b9caac6
Jan <janfrei01@gmail.com>
2020-02-11 17:02:12 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    env: {
+        es6: true,
+        node: true,
+        jquery: true,
+        browser: true,
+    },
+    extends: [
+        'airbnb-base',
+        'eslint:recommended',
+        'plugin:prettier/recommended',
+    ],
+    plugins: ['prettier'],
+    rules: {
+        'prettier/prettier': ['warn'],
+        'func-names': 'off',
+        'prefer-rest-params': 'off',
+        'no-param-reassign': 'off',
+        'no-console': 'off',
+    },
+    globals: {
+        CinemaWindow: true,
+        Map: true,
+        west: true,
+        wman: true,
+        scriptUpdater: true,
+        Premium: true,
+        UserMessage: true,
+        JobWindow: true,
+        GameLoader: true,
+        Chat: true,
+        PlayerProfileMain: true,
+        Character: true,
+        ItemManager: true,
+    },
+};
---
+++ README.md
@@ -0,0 +1,10 @@
+# tw-perseus-toolkit
+
+Userscript for The West. Provides useful features:
+
+-   Highlight Silver / Gold Jobs (it doesn't search them by it's own)
+-   Button to skip cinema videos after 5 seconds
+-   Zoom the map in and out (hover the minimap icon on the top right and scroll up / down)
+-   Disable premium notifications for energy (refill) and automation.
+-   Button in job windows which allows you to start 9x 15 second jobs with one click.
+-   Hide "Drawing Map" flag
